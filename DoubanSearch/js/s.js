fd720989ce0561b601d46c558b72163093846e7d
ccbility <871892708@qq.com>
2018-06-06 14:58:59 +0800

    修改默认为书籍；新增回车监听

--- DoubanSearch/js/s.js
+++ DoubanSearch/js/s.js
@@ -1,12 +1,21 @@
 $('input[type=submit]').click(function () {
-    var search_item = $('input[name=search_item]').val();
-    var is_book = $('input[name=is_book]:checked').val();
-
-    if (search_item) {
-        if (is_book == undefined) {
-            chrome.tabs.create({"url": 'https://movie.douban.com/subject_search?search_text=' + search_item});
-        } else {
-            chrome.tabs.create({"url": 'https://book.douban.com/subject_search?search_text=' + search_item});
-        }
-    }
+    startSearch();
 });
+$(document).keyup(function(event){  
+  if(event.keyCode ==13){  
+     startSearch();
+  }  
+});
+
+function startSearch() {
+	var search_item = $('input[name=search_item]').val();
+	    var is_movie = $('input[name=is_movie]:checked').val();
+
+	    if (search_item) {
+	        if (is_movie == undefined) {
+	            chrome.tabs.create({"url": 'https://book.douban.com/subject_search?search_text=' + search_item});
+	        } else {
+	            chrome.tabs.create({"url": 'https://movie.douban.com/subject_search?search_text=' + search_item});
+	        }
+	    }
+}
